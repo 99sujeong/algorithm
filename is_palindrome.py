c7467956eed71158d5be0a16fded0302bfe02272
99sujeong <66475575+99sujeong@users.noreply.github.com>
2021-03-04 20:22:50 +0900

    is_palindrome_practice

---
+++ is_palindrome.py
@@ -0,0 +1,12 @@
+def is_palindrome(word):
+    if len(word) < 2:
+        return True
+    if word[0] != word[-1]:
+        return False
+    return is_palindrome(word[1:-1])
+
+print(is_palindrome("racecar"))
+print(is_palindrome("stars"))
+print(is_palindrome("토마토"))
+print(is_palindrome("kayak"))
+print(is_palindrome("hello"))

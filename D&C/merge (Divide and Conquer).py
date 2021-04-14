ec878aa64f4b3bbfc67b086498006172b67a1cce
99sujeong <66475575+99sujeong@users.noreply.github.com>
2021-04-14 23:37:52 +0900

    D&C+simple_merge_practice

---
+++ D&C/merge (Divide and Conquer).py
@@ -0,0 +1,12 @@
+def merge(list1, list2):
+    list = list1 + list2
+    
+    return sorted(list)
+    
+# 테스트
+print(merge([1],[]))
+print(merge([],[1]))
+print(merge([2],[1]))
+print(merge([1, 2, 3, 4],[5, 6, 7, 8]))
+print(merge([5, 6, 7, 8],[1, 2, 3, 4]))
+print(merge([4, 7, 8, 9],[1, 3, 6, 10]))
